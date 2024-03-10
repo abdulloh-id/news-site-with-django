437ebaedd609bf9794e4f50abced9d56b871b332
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2024-03-11 04:41:27 +0500

    static va media configuratsiyalar setting file ga set up qilindi

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.conf import settings
+from django.views.static import serve
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newsApp.urls'))
+]
+
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
