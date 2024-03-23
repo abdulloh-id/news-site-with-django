ce8b58c564abf2fd791e9baa4a62698fb816b7cd
abdulloh-id <128278116+abdulloh-id@users.noreply.github.com>
2024-03-23 05:16:17 +0500

    Integrated the newsapi

--- crypto/views.py
+++ crypto/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from newsapi import NewsApiClient
+from dotenv import load_dotenv
+import os
+import datetime
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 # Create your views here.
 class CryptoPageView(TemplateView):
 	template_name = 'crypto.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		today = datetime.datetime.now().date()
+		yesterday = today - datetime.timedelta(days=1)
+
+		newsapi = NewsApiClient(api_key=API_KEY)
+
+		# top_headlines = newsapi.get_top_headlines(
+		# 										q='bitcoin',
+		# 										language='en')
+		
+		all_articles = newsapi.get_everything(
+											q='bitcoin',
+											from_param=yesterday,
+											to=today,
+											language='en',
+											sort_by='relevancy')
+
+		#context['top_headlines'] = top_headlines
+		context['all_articles'] = all_articles
+
+		return context
